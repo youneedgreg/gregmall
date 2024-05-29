7cbd8d937ac5c94b0c57e22cce866d939afbd0f8
youneedgreg <gregorytemwa1212@gmail.com>
2024-05-29 18:54:41 +0300

    navbar settings

---
+++ OneDrive/Desktop/mtaamallreact/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+<header class="header">
+
+    <div class="alert">
+      <div class="container">
+        <p class="alert-text">Where we unleash the power of endless possibilities with every click.</p>
+      </div>
+    </div>
+
+    <div class="header-top" data-header>
+      <div class="container">
+
+        <button class="nav-open-btn" aria-label="open menu" data-nav-toggler>
+          <span class="line line-1"></span>
+          <span class="line line-2"></span>
+          <span class="line line-3"></span>
+        </button>
+
+        <div class="input-wrapper">
+          <input type="search" name="search" placeholder="Search product" class="search-field">
+
+          <button class="search-submit" aria-label="search">
+            <ion-icon name="search-outline" aria-hidden="true"></ion-icon>
+          </button>
+        </div>
+
+        <a href="#" class="logo">
+          <h1>MtaaMall</h1>
+        </a>
+
+
+        <div class="header-actions">
+
+          <button class="header-action-btn" aria-label="user">
+            <ion-icon name="person-outline" aria-hidden="true" aria-hidden="true"></ion-icon>
+          </button>
+
+          <button class="header-action-btn" aria-label="favourite item">
+            <ion-icon name="star-outline" aria-hidden="true" aria-hidden="true"></ion-icon>
+
+            <span class="btn-badge">0</span>
+          </button>
+
+        </div>
+
+      </div>
+    </div>
+
+  </header>
